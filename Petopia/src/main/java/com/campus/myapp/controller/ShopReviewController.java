888c128cd20765394d1d74cc9af57673588952f0
ParkYunri <yoyoyo066@naver.com>
2022-05-20 11:34:48 +0900

    map 수정

--- Petopia/src/main/java/com/campus/myapp/controller/ShopReviewController.java
+++ Petopia/src/main/java/com/campus/myapp/controller/ShopReviewController.java
@@ -1,7 +1,6 @@
 package com.campus.myapp.controller;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -80,7 +78,7 @@ public class ShopReviewController {
 	public int deleteReview(int reviewno, HttpSession session) {
 		String filename =  service.selectFile(reviewno);
 		
-		String path = session.getServletContext().getRealPath("/upload/review");
+		String path = session.getServletContext().getRealPath("/upload");
 		if(filename!=null) {
 			File f = new File(path,filename);
 			if(f!=null && f.exists()) {
