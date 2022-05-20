fd39a39b64f64c7d4b1a9eea6cfb19ccfb6a2111
ParkYunri <yoyoyo066@naver.com>
2022-05-20 11:25:55 +0900

    commit

--- Petopia/src/main/java/com/campus/myapp/controller/ShopReviewController.java
+++ Petopia/src/main/java/com/campus/myapp/controller/ShopReviewController.java
@@ -31,7 +31,7 @@ public class ShopReviewController {
 	@PostMapping("writeOk")
 	public int writeOk(@RequestParam("filename") MultipartFile filename, ShopReviewVO vo, HttpSession session) {
 		vo.setUserid((String) session.getAttribute("logId"));
-		String path = session.getServletContext().getRealPath("/upload/review");
+		String path = session.getServletContext().getRealPath("/upload");
 		try {
 			if(!filename.isEmpty()) {
 				filename.transferTo(new File(path+"/"+filename.getOriginalFilename()));
@@ -54,7 +54,7 @@ public class ShopReviewController {
 	@PostMapping("editOk")
 	public int updateReview(@RequestParam("filename") MultipartFile filename,ShopReviewVO vo, HttpSession session) {
 		vo.setUserid((String)session.getAttribute("logId"));
-		String path = session.getServletContext().getRealPath("/upload/review");
+		String path = session.getServletContext().getRealPath("/upload");
 
 		try {
 			if(vo.getDeleteFile()!=null && vo.getDeleteFile()!="") {
